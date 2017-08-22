d10a6b2661bcf61b417208581701125afc0a8b74
Joseph Calabrese <joseph091@dhcp-v119-141.mobile.uci.edu>
2017-08-22 10:36:48 -0700

    update

--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ var request= require("request")
 var omdbJs= require("./omdb.js");
 
 //node liri.js my-tweets
-function execute (command, song){
+// function execute (command, song){
 if(command[2] == "my-tweets"){
   	twit.get('statuses/user_timeline', function(error, tweets, response) {
   	for (var i = 0 ; i< 20; i++)
@@ -62,9 +62,25 @@ else if(command[2]== "spotify-this-song"){
 else if (command[2]== "do-what-it-says"){
 
 	fs.readFile("random.txt", "utf8", function(err, data) {
-	data = data.split(", ");
-    console.log(data);
-	}
+		data = data.split(",");
+		var newCommand = data[0];
+		var newSong= data[1];
+    	console.log(data);
+    	if (newCommand == "spotify-this-song"){
+   //  		var input=""
+			// for (var i=3; i<command.length; i++){
+			// input= input + "+" + command[i];	
+			// };
+ 				spot.search({ type: 'track', query: newSong }, function(err, data) {
+ 					for (var i=0; i<data.tracks.items.length; i ++){
+ 					console.log();
+    				console.log("Name: " + data.tracks.items[i].name);
+    				console.log("Artist: " + data.tracks.items[i].artists[0].name);
+    				console.log("Album: " + data.tracks.items[i].album.name);
+					}
+				});
+    	}
 });
 }
-execute(command, song);
+
+// 
